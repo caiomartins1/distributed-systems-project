9be7757a5f661b746d803848144a881ff3d1b1fd
Caio Martins <dev.caiomartins@gmail.com>
2021-04-29 23:40:28 +0100

    added client input method signatures

--- source/src/pt/ubi/di/interfaces/ManagerClientInterface.java
+++ source/src/pt/ubi/di/interfaces/ManagerClientInterface.java
@@ -5,5 +5,7 @@ import java.rmi.RemoteException;
 
 public interface ManagerClientInterface extends Remote {
     public void printOnClient(String s) throws RemoteException;
+    public String readStringClient() throws RemoteException;
+    public int readIntClient() throws RemoteException;
 
 }
