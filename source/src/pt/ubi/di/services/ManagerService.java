0823cb1dc9e3bfee336dc6182f0b63a8f80f19e4
Caio Martins <dev.caiomartins@gmail.com>
2021-04-30 19:23:34 +0100

    Added filters logic for option 4

--- source/src/pt/ubi/di/services/ManagerService.java
+++ source/src/pt/ubi/di/services/ManagerService.java
@@ -4,6 +4,8 @@ import pt.ubi.di.model.Part;
 import pt.ubi.di.utils.FileUtils;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ManagerService {
 
@@ -15,5 +17,88 @@ public class ManagerService {
     public void deletePart(ArrayList<Part> parts, int index) {
         parts.remove(index);
         FileUtils.saveParts(parts);
+    }
+
+    public String listByTypeAlpha(ArrayList<Part> p) {
+        String s = "";
+
+        if (p.isEmpty()) {
+            s = "No parts registered yet!";
+        } else {
+            Collections.sort(p, new Comparator<Part>() {
+                @Override
+                public int compare(Part s1, Part s2) {
+                    return s1.getType().compareToIgnoreCase(s2.getType());
+                }
+            });
+
+            for(Part part: p) {
+                s += part.beautifyOutput() + "\n";
+            }
+        }
+
+        return s;
+    }
+
+    public String listByBuyPrice(ArrayList<Part> p) {
+        String s = "";
+
+        if (p.isEmpty()) {
+            s = "No parts registered yet!";
+        } else {
+            Collections.sort(p, new Comparator<Part>() {
+                @Override
+                public int compare(Part s1, Part s2) {
+                    return s1.getBuyPrice() - s2.getBuyPrice() < 1 ? -1 : 1;
+                }
+            });
+
+            for(Part part: p) {
+                s += part.beautifyOutput() + "\n";
+            }
+        }
+
+        return s;
+    }
+
+    public String listBySellPrice(ArrayList<Part> p) {
+        String s = "";
+
+        if (p.isEmpty()) {
+            s = "No parts registered yet!";
+        } else {
+            Collections.sort(p, new Comparator<Part>() {
+                @Override
+                public int compare(Part s1, Part s2) {
+                    return s1.getSellPrice() - s2.getSellPrice() < 1 ? -1 : 1;
+                }
+            });
+
+            for(Part part: p) {
+                s += part.beautifyOutput() + "\n";
+            }
+        }
+
+        return s;
+    }
+    public String listByStockItems(ArrayList<Part> p) {
+        String s = "";
+
+        if (p.isEmpty()) {
+            s = "No parts registered yet!";
+        } else {
+            Collections.sort(p, new Comparator<Part>() {
+                @Override
+                public int compare(Part s1, Part s2) {
+                    return s2.getItems().size() - s1.getItems().size() < 1 ? -1 : 1;
+                }
+            });
+
+            for(Part part: p) {
+                s += part.beautifyOutput() + "\n";
+            }
+        }
+
+        return s;
     }
 }
